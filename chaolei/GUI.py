35700bb7a4962580d1c867b7a87836bb85daef75
Playmaker <chaoleicai@gmail.com>
2019-03-26 11:44:29 +0100

    gui destroy correctly

--- chaolei/GUI.py
+++ chaolei/GUI.py
@@ -4,53 +4,89 @@ import tkinter as tk
 from tkinter import messagebox
 import pickle
 
+
+class LoginFrame(tk.Frame):#genere une frame pour le login screen
+    def __init__(self, master):
+        super().__init__(master)
+        self.login_statut = False
+
+
+        self.label_username = tk.Label(self, text="Username")
+        self.label_servername = tk.Label(self, text="Server name")
+        self.label_port = tk.Label(self, text="Port")
+
+
+        self.entry_username = tk.Entry(self)
+        self.entry_servername = tk.Entry(self)
+        self.entry_port = tk.Entry(self)
+
+        self.label_username.grid(row=0)
+        self.label_servername.grid(row=1)
+        self.label_port.grid(row=2)
+        
+        self.entry_username.grid(row=0, column=1)
+        self.entry_servername.grid(row=1, column=1)
+        self.entry_port.grid(row=2, column=1)
+
+        self.logbtn = tk.Button(self, text="Login", command=self._login_btn_clicked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+
+    def get_statut(self):
+    	return self.login_statut
+
+    def get_username(self):
+    	return self.entry_username.get()
+    def get_servername(self):
+    	return self.entry_servername.get()
+    def get_port(self):
+    	return self.entry_port.get()
+
+
+    def _login_btn_clicked(self):
+        # print("Clicked")
+        username = self.entry_username.get()
+        servername = self.entry_servername.get()
+        port = self.entry_port.get()
+        # print(username, servername)
+
+        if username == "john" and servername == "servername":
+            tk.messagebox.showinfo("Login info", "Welcome John")
+        else:
+            tk.messagebox.showerror("Login error", "Incorrect username")
+
 def usr_login():
     pass    
 
-def on_hit():
-    tk.messagebox.showinfo(title= "Name requiered",message="Please enter your name")
-
-class GUI(object):
+class GUI:
     """docstring for GUI"""
-    def __init__(self, arg):
-        super(GUI, self).__init__()
-        self.arg = arg
+    def __init__(self):
         self.window = tk.Tk()
         self.window.title("Chat client")
         self.window.geometry("400x600")
-
-        self.pseudo = tk.StringVar()
-        self.pseudo.set('hhhhhh')
-
-        pseudoInput = tk.Label(self.window,
-            textvariable = self.pseudo,
-            bg = 'grey', width = 15, height = 2)
-
-        pseudoInput.pack()
-
-
-        tk.Label(self.window, text='User name: ').place(x=50, y= 150)
-        tk.Label(self.window, text='Server name: ').place(x=50, y= 170)
-        tk.Label(self.window, text='Port: ').place(x=50, y= 190)
+        #login step
+        loginframe = LoginFrame(self.window)
+        while(loginframe.get_statut() != "ok"):
+        	loginframe.login_statut = input("put")
         
-        var_usr_userName = tk.StringVar()
-        var_usr_userName.set('bk211')
-        entry_usr_userName = tk.Entry(self.window, textvariable=var_usr_userName)
-        entry_usr_userName.place(x=160, y=150)
-    
-        var_usr_serverName = tk.StringVar()
-        var_usr_serverName.set('Pablo.Rauzy.com')
-        entry_usr_serverName = tk.Entry(self.window, textvariable=var_usr_serverName)
-        entry_usr_serverName.place(x=160, y=170)
+        #print("out")
+        self.username = loginframe.get_username()
+        self.servername = loginframe.get_servername()
+        self.port = loginframe.get_port()
 
-        var_usr_port = tk.StringVar()
-        var_usr_port.set('4567')
-        entry_usr_port = tk.Entry(self.window, textvariable=var_usr_port)
-        entry_usr_port.place(x=160, y=190)
-        confirmButton=tk.Button(self.window, text='hit me', command=on_hit)
-        confirmButton.place(x = 150, y = 220)
-
+        loginframe.destroy()
+        print("port:",self.username)
+        print("servername:",self.servername)
+        print("port:",self.port)
         self.window.mainloop()
 
+
+class Application:
+	def __init(self):
+		_gui = GUI()
+		
+
 if __name__ == '__main__':
-    gui = GUI(123)
+    Application = GUI()
---
+++ chaolei/login_example.py
@@ -0,0 +1,46 @@
+from tkinter import *
+import tkinter.messagebox as tm
+
+
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_username = Label(self, text="Username")
+        self.label_servername = Label(self, text="Server name")
+        self.label_port = Label(self, text="Port")
+
+
+        self.entry_username = Entry(self)
+        self.entry_servername = Entry(self)
+        self.entry_port = Entry(self)
+
+        self.label_username.grid(row=0, sticky=E)
+        self.label_servername.grid(row=1, sticky=E)
+        self.label_port.grid(row=2, sticky=E)
+        
+        self.entry_username.grid(row=0, column=1)
+        self.entry_servername.grid(row=1, column=1)
+        self.entry_port.grid(row=2, column=1)
+
+        self.logbtn = Button(self, text="Login", command=self._login_btn_clicked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clicked(self):
+        # print("Clicked")
+        username = self.entry_username.get()
+        servername = self.entry_servername.get()
+        port = self.entry_port.get()
+        # print(username, servername)
+
+        if username == "john" and servername == "servername":
+            tm.showinfo("Login info", "Welcome John")
+        else:
+            tm.showerror("Login error", "Incorrect username")
+
+
+root = Tk()
+lf = LoginFrame(root)
+root.mainloop()
