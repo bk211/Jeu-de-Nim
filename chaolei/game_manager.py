ad99e84722961e2eb073e95a0b8e218c036e54f4
Playmaker <chaoleicai@gmail.com>
2019-04-01 12:29:46 +0200

    add play card feature

--- chaolei/game_manager.py
+++ chaolei/game_manager.py
@@ -17,16 +17,34 @@ class Game_data_manager():
     def reset_pile(self):
         self.pile_table = 0
 
-    def check_valid_card(self, card):
+    def update_pile(self, card):
+        self.pile_table += card
+
+    def check_valid_card_from_deck(self, card):
         return card in self.cards_deck
 
+    def check_valid_card_from_hand(self, player_name, card):
+        return card in self.players_hands[player_name]
+
     def remove_card_from_deck(self, card):
-        if self.check_valid_card(card):
+        if self.check_valid_card_from_deck(card):
             self.cards_deck.remove(card)
             return True
         return False
 
-    def new_player(self, player_name):
+    def remove_card_from_hand(self, player_name, card):
+        if self.check_valid_card_from_hand(player_name, card):
+            self.cards_bin.append(self.players_hands[player_name].pop(card))
+            return True
+        return False
+
+    def play_hand(self, player_name, card):
+        if self.remove_card_from_hand(player_name, card):
+            self.update_pile(card)
+            return True
+        return False
+
+    def add_new_player(self, player_name):
         self.players_hands[player_name] = []
         self.players_wallets[player_name] = WALLET_INIT_AMOUNT
 
@@ -48,8 +66,8 @@ class Game_data_manager():
 def main():
     _g = Game_data_manager()
     print(_g.cards_deck)
-    _g.new_player("player1")
-    _g.new_player("player2")
+    _g.add_new_player("player1")
+    _g.add_new_player("player2")
     print(_g.players_hands)
     _g.give_cards_to_all()
     print(_g.cards_deck)
